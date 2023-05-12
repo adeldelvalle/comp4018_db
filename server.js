e5f5cf33c7f0e1d71a0860b42d30150dfaac331e
Adel <adel.delvalle@upr.edu>
2023-05-11 20:24:43 -0400

    a

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 // Serve static files from the 'public' directory
+app.use('/mynode', express.static(path.join(__dirname, 'public')));
+
 app.get('/mynode', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
